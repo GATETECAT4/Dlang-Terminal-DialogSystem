10730a19dd21761cc6d5eb591cc055dd54a94d40
GatoPresidente <95552148+GATETECAT4@users.noreply.github.com>
2023-08-16 15:25:57 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Este script de bash es para no tener que escribir todo esto a cada rato
+# Este script de bash es para no tener que escribir todo esto de aquí abajo a cada rato al probar y editar
+# Cambia el "dmd" con el comando de tu compilador preferido para que puedas usar ese compilador en vez de dmd (obvio)
 
 dmd main.d VinnieCodes/logs.o VinnieCodes/names.o VinnieCodes/faces.o
+
+# NOTA: ESTE SCRIPT ÚNICAMENTE COMPILA EL CÓDIGO PRINCIPAL JUNTO A LOS ARCHIVOS QUE NECESITA, NO HACE NADA MÁS
